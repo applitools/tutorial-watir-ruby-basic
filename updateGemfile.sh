a1d421a0780d5c25b9df71bbe323316cef43a4bb
artem <artem.borodavka@applitools.com>
2021-05-24 12:46:09 +0300

    fix command getting latest watir version

--- updateGemfile.sh
+++ updateGemfile.sh
@@ -1,6 +1,6 @@
 EYES=$(gem search -e eyes_selenium | grep eyes_selenium | grep -E -o "[0-9]+.[0-9]+.[0-9]+")
 echo "eyes_selenium version: $EYES"
-WATIR=$(gem search -e watir | grep watir | grep -E -o "[0-9]+.[0-9]+.[0-9]+")
+WATIR=$(gem search -e watir | grep watir | grep -E -o "[0-9]+.[0-9]+.[0-9]+" | head -1)
 echo "watir version: $WATIR"
 bundle remove eyes_selenium
 bundle remove watir
